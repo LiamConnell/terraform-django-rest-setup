c1cd9da34e6cd2d8218e623a13f9c296f3462e1a
Liam Connell <liam.connell@auth0.com>
2018-11-11 12:55:58 -0300

    ignore

--- call_ansible.sh
+++
@@ -1,1 +0,0 @@
-yes yes | ansible-playbook -u ubuntu -i '34.211.39.106,' --private-key ~/.ssh/liams_key.pem provision.yml
--- script.sh
+++
@@ -1,1 +0,0 @@
-alias ssh_alias='ssh -i ~/.ssh/liams_key.pem ubuntu@34.221.56.63'
